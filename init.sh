ee548e35c9ec67410a511000a42ad3ccac9ab21b
trevor-poirier <117388844+trevor-poirier@users.noreply.github.com>
2025-11-24 16:14:20 -0600

    Add files via upload

--- init.sh
+++ init.sh
@@ -13,6 +13,7 @@ printf '\nQ3\n'
 printf '\nQ4\n'
 kubectl create namespace ns-quota1
 kubectl -n ns-quota1 create deployment resource-deploy --image=nginx --replicas=1
+kubectl -n ns-quota1 set resources deployment/resource-deploy --requests=cpu=100m --limits=memory=650Mi,cpu=200m
 kubectl apply -f ./CKAD-Exam-Questions/Q4/resource-quota.yaml
 
 # Q5
